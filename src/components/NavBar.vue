a8d664843be57e47dd6970692d33aa196cbf54c2
Shaher Sarairah <shahersar@icloud.com>
2021-12-05 08:26:20 +0200

    Updates

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,19 +1,19 @@
 <template>
   <b-navbar toggleable="lg" type="dark" variant="dark">
-    <b-navbar-brand href="/">E-Wallet</b-navbar-brand>
+    <b-navbar-brand><router-link to="/">E-Wallet</router-link></b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav v-if="isLoggedIn && isAdmin">
-        <b-nav-item href="/admin" >Chart</b-nav-item>
-        <b-nav-item href="/admin/payment-methods" >PaymentMethods</b-nav-item>
-        <b-nav-item href="/admin/users" >Users</b-nav-item>
-        <b-nav-item href="/admin/transactions" >Transactions</b-nav-item>
+      <b-navbar-nav v-if="isLoggedIn">
+        <b-nav-item><router-link to="/admin">Dashboard</router-link></b-nav-item>
+        <b-nav-item><router-link to="/admin/payment-methods">PaymentMethods</router-link></b-nav-item>
+        <b-nav-item><router-link to="/admin/users">Users</router-link></b-nav-item>
+        <b-nav-item><router-link to="/admin/transactions">Transactions</router-link></b-nav-item>
       </b-navbar-nav>
       <b-navbar-nav v-if="!isLoggedIn">
-        <b-nav-item href="/login">Login</b-nav-item>
-        <b-nav-item href="/register">Register</b-nav-item>
+        <b-nav-item><router-link to="/login">Login</router-link></b-nav-item>
+        <b-nav-item><router-link to="/register">Register</router-link></b-nav-item>
       </b-navbar-nav>
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto" v-if="isLoggedIn">
